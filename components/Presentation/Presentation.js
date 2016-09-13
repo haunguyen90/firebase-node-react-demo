38256e33dbf43a4e3429ee83abda8839b4bc0d46
Hau Nguyen <nltrunghau@gmail.com>
2016-09-14 03:51:10 +0700

    finished deck settings

--- components/Presentation/Presentation.js
+++ components/Presentation/Presentation.js
@@ -6,11 +6,15 @@ import {Link, withRouter} from 'react-router';
 import {PageHeader, Row, Col, Tabs, Tab} from 'react-bootstrap';
 import {ENUMS} from '~/lib/_required/enums.js';
 
+import DeckSettings from './deckSettings.js';
+
 class Presentation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      presentationTabActive: 1
+      observerAuth: null,
+      presentationTabActive: 1,
+      deckObject: {}
     };
 
     this.handlePresentationTabSelect = this.handlePresentationTabSelect.bind(this);
@@ -19,6 +23,39 @@ class Presentation extends React.Component {
 
   handlePresentationTabSelect(key){
     this.setState({presentationTabActive: key});
+  }
+
+  componentDidMount(){
+    if(!this.props.params.id)
+      return false;
+
+    const observerAuth = firebase.auth().onAuthStateChanged((user) => {
+      let presentationRef = firebase.database().ref('decks/' + this.props.params.id);
+      if(user){
+        presentationRef.on("value", (result) => {
+          const deckObject = result.val();
+          if(deckObject.uid == user.uid){
+            deckObject.id = this.props.params.id;
+            this.setState({deckObject: deckObject});
+          }else{
+            alert("The presentation is not belong to you.");
+            // TODO
+            // Need to discuss the flow for this case
+          }
+        });
+      }else{
+        presentationRef.off();
+      }
+    });
+    this.setState({observerAuth: observerAuth});
+  }
+
+  componentWillUnmount(){
+    const observerAuth = this.state.observerAuth;
+    if(observerAuth && typeof observerAuth == 'function'){
+      // Unsubscribe auth change
+      observerAuth();
+    }
   }
 
   render(){
@@ -30,7 +67,11 @@ class Presentation extends React.Component {
         <Row>
           <Col xs={12} className="presentation-tabs">
             <Tabs activeKey={this.state.presentationTabActive} onSelect={this.handlePresentationTabSelect} id="presentationTabs">
-              <Tab eventKey={1} title="SETTINGS">SETTINGS</Tab>
+              <Tab eventKey={1} title="SETTINGS">
+                <DeckSettings
+                  deckObject={this.state.deckObject}
+                />
+              </Tab>
               <Tab eventKey={2} title="SLIDES">SLIDES</Tab>
               <Tab eventKey={3} title="SHARE">SHARE</Tab>
             </Tabs>
