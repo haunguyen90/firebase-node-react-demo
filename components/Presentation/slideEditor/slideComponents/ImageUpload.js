392ea4ee714e70f1855e2c642f1d88dabe3ed83e
Hoa Dinh <dinhnguyentrunghoa@gmail.com>
2016-10-05 17:26:05 +0700

    - Show Modal Upload  - Show libary image  - Save UserAssets to db  - Select image and save to Image ccomponent

---
+++ components/Presentation/slideEditor/slideComponents/ImageUpload.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import {Row, Button, } from 'react-bootstrap';
+import {isMounted} from '~/lib/react/reactLib.js';
+import {findIndex} from 'underscore';
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUploading: false,
+      uploadPercent: 0
+    }
+  }
+
+  // Check the browser support drag n drop
+  checkDragNDropFeature() {
+      var div = document.createElement('div');
+      return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+  }
+
+  // If the browser doesn't support drag n drop don't show the message to guide the user.
+  renderUploadText() {
+    if (this.checkDragNDropFeature()) {
+      return (
+        <span class="box__dragndrop">Drop file here to Upload OR </span>
+      );
+    }
+  }
+
+  updatePhotoURLToDB(downloadURL){
+    const {selectedSlide, keyId, deckId, getSlides} = this.props;
+
+    // Upload image completed. Save data to UserAsset and Open LIBRARY tab
+    this.props.handleUploadImageTabSelect(2);
+    let newUserAssetKey = firebase.database().ref().child('userAssets').push().key;
+    firebase.database().ref('userAssets/' + newUserAssetKey).set({
+      uid: firebase.auth().currentUser.uid,
+      type: "IMAGE",
+      url : downloadURL
+    });
+    
+  }
+
+  onInputFileChange(evt){
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    const pattern = /^image\/(gif|jpg|jpeg|tiff|png)$/i;
+
+    const storageRef = firebase.storage().ref();
+    const file = evt.target.files[0];
+
+    if(!pattern.test(file.type)){
+      this.props.handleAlertShow("File type not support");
+      return false;
+    }
+
+    const metadata = {
+      'contentType': file.type
+    };
+
+    // Upload file and metadata to the object, each user has its folder to store image
+    if (firebase.auth().currentUser) {
+      const uploadTask = storageRef.child('images/' + firebase.auth().currentUser.uid + '/' +file.name).put(file, metadata);
+      const instance = this;
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          if(isMounted(instance)){
+            this.setState({isUploading: true});
+            this.setState({uploadPercent: progress});
+          }
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+          if (firebase.storage.TaskState.SUCCESS) {
+
+          }
+        }, (error) => {
+          switch (error.code) {
+            case 'storage/unauthorized':
+              console.error(error);
+              // User doesn't have permission to access the object
+              break;
+
+            case 'storage/canceled':
+              console.error(error);
+              // User canceled the upload
+              break;
+
+            case 'storage/unknown':
+              console.error(error);
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        }, () => {
+          // Upload completed successfully, now we can get the download URL
+          const downloadURL = uploadTask.snapshot.downloadURL;
+          setTimeout(() =>{
+            if(isMounted(instance)){
+              this.setState({isUploading: false});
+              this.setState({uploadPercent: 0});
+              this.updatePhotoURLToDB(downloadURL);
+            }
+          }, 175);
+        })
+    } else {
+      //TODO: return failed if user is not login
+      console.log('No user is login');
+    };
+  }
+
+
+  render() {
+    return (
+      <Row className="imageupload-component">
+        <form className="box" method="post" action="" encType="multipart/form-data">
+          <div className="box__input">
+            <input className="box__file" onChange={this.onInputFileChange.bind(this)} type="file" name="file" id="fileInput" accept='image/*'/>
+            <p>{this.renderUploadText()}</p>
+            <Button className="uploadButton" bsStyle="primary" block>
+              <label htmlFor="fileInput" className="fileInputLabel">
+                <i className="fa fa-upload"></i>
+                <span className="label-text">UPLOAD</span>
+              </label>
+            </Button>
+          </div>
+          <div className="box__uploading">Uploading&hellip;</div>
+          <div className="box__success">Done!</div>
+          <div className="box__error">Error! <span></span>.</div>
+        </form>
+      </Row>
+    )
+  }
+}
+
+ImageUpload.propTypes = {
+  selectedSlide : React.PropTypes.object,
+  keyId : React.PropTypes.number,
+  deckId : React.PropTypes.string,
+  getSlides : React.PropTypes.func,
+  closeShareWindow : React.PropTypes.func,
+  handleUploadImageTabSelect : React.PropTypes.func
+}
+
+export default ImageUpload;
---
+++ components/Presentation/slideEditor/slideComponents/UploadImageModal.js
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import {Modal, Row, Tabs, Tab} from 'react-bootstrap';
+import ImageUpload from './ImageUpload.js';
+import ImageLibrary from  './ImageLibrary.js';
+
+class UploadImageModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        imageUploadActiveTab : 1
+    };
+  }
+
+  handleUploadImageTabSelect(key) {
+    this.setState({imageUploadActiveTab: key});
+  }
+
+  render(){
+    return  (
+      <Modal show={this.props.showModal} onHide={this.props.closeShareWindow} className="uploadimage-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>Add Image</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <Row>
+              <div>
+                <Tabs id="uploadingImageTabs" activeKey={this.state.imageUploadActiveTab} onSelect={this.handleUploadImageTabSelect.bind(this)}>
+                  <Tab eventKey={1} title="UPLOAD">
+                    <ImageUpload {...this.props}
+                            handleUploadImageTabSelect = {this.handleUploadImageTabSelect.bind(this)}/>
+                  </Tab>
+                  <Tab eventKey={2} title="LIBRARY">
+                    <ImageLibrary {...this.props}/>
+                  </Tab>
+                </Tabs>
+              </div>
+            </Row>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+    )
+  }
+}
+
+UploadImageModal.propTypes = {
+  showModal : React.PropTypes.bool,
+  closeShareWindow : React.PropTypes.func
+}
+
+export default UploadImageModal
